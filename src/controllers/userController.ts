154d48222e1366007fd8c88faf210b10920f7563
Florian <florian.barroso@edu.esee-it.fr>
2025-12-01 14:29:47 +0100

    feat hide user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -15,6 +15,16 @@ export async function getUser(req : any, res: any) : Promise<UserInterface> {
     return userCRUD.getOne(req, res);
 }
 
+export async function getActiveUser(req : any, res: any) : Promise<UserInterface[]> {
+    try {
+        const users : UserInterface[] = await User.find({isActive : true});
+        if(users && users.length === 0) return ApiResponse.notFound(res, "No users found.");
+        return ApiResponse.success(res, users);
+    } catch (err : any) {
+        return ApiResponse.serverError(res);
+    }
+}
+
 export async function createUser(req:any, res:any) : Promise<UserInterface> {
     if(!verifyId(req.body.role)) return ApiResponse.invalidId(res);
 
@@ -33,6 +43,24 @@ export async function updateUser(req:any, res:any) : Promise<UserInterface> {
         return ApiResponse.notFound(res ,"No role found.");
     }
     return userCRUD.update(req,res);
+}
+
+export async function hideUser(req:any, res:any) : Promise<UserInterface> {
+    try {
+        const userId = req.params.id;
+        if(!verifyId(userId)) return ApiResponse.invalidId(res);
+
+        const user = await User.findOneAndUpdate(
+            { _id: userId },
+            {isActive : req.body.isActive}
+        );
+        if(!user) return ApiResponse.notFound(res, "No user found.");
+
+        return ApiResponse.success(res, user);
+    } catch (err : any) {
+        if(err.code === 11000) return ApiResponse.conflict(res, "Duplicate key.");
+        return ApiResponse.serverError(res);
+    }
 }
 
 export async function deleteUser(req:any, res:any) : Promise<UserInterface> {
