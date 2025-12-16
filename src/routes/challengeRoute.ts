75b68cfe9b7998629708ff28d78cb4c7f51d8678
alexy103 <alexy103@hotmail.fr>
2025-12-16 17:33:11 +0100

    Add basic filters

--- src/routes/challengeRoute.ts
+++ src/routes/challengeRoute.ts
@@ -1,15 +1,35 @@
 import express, { json } from 'express';
-import { getChallenges, getChallenge, createChallenge, updateChallenge, deleteChallenge } from '../controllers';
 import { authorizeRoles } from '../utils';
+import {
+	approveChallenge,
+	createChallenge,
+	deleteChallenge,
+	getApprovedChallenges,
+	getChallenge,
+	getChallenges,
+	getChallengesByDifficulty,
+	getChallengesByDuration,
+	getChallengesByExercise,
+	getPopularChallenges,
+	getUnapprovedChallenges,
+	updateChallenge,
+} from '../controllers';
 
 const router = express.Router();
 
-// Lecture publique
+router.put('/approve/:id', approveChallenge);
+router.get('/unapproved', getUnapprovedChallenges);
+router.get('/approved', getApprovedChallenges);
+router.get('/popular/:limit', getPopularChallenges);
+router.get('/difficulty/:level', getChallengesByDifficulty);
+router.get('/exercise/:id', getChallengesByExercise);
+router.get('/duration/:min/:max', getChallengesByDuration);
+
 router.get('/', getChallenges);
 router.get('/:id', getChallenge);
 // Création, modification, suppression : owner (propriétaire) et customer (utilisateur)
-router.post('/', authorizeRoles(["owner", "customer"]), json(), createChallenge);
-router.put('/:id', authorizeRoles(["owner", "customer"]), json(), updateChallenge);
-router.delete('/:id', authorizeRoles(["owner", "customer"]), deleteChallenge);
+router.post('/', authorizeRoles(['owner', 'customer']), json(), createChallenge);
+router.put('/:id', authorizeRoles(['owner', 'customer']), json(), updateChallenge);
+router.delete('/:id', authorizeRoles(['owner', 'customer']), deleteChallenge);
 
 export default router;
